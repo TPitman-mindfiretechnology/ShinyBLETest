5456c22ff93b1d6e3614ed1dc8693e7312ccbd64
tpitman <tony@shatalmic.com>
2023-05-10 07:20:59 -0600

    Updated project to show a crash on Android

--- ShinyBLETest/MainPage.xaml.cs
+++ ShinyBLETest/MainPage.xaml.cs
@@ -1,7 +1,20 @@
 ﻿using Shiny.BluetoothLE;
 using System.Linq;
+using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace ShinyBLETest;
+
+#if ANDROID
+internal class Android12PlusPermissions : BasePlatformPermission
+{
+	public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
+		new List<(string permissions, bool isRuntime)>
+		{
+			("android.permission.BLUETOOTH_SCAN", true),
+			("android.permission.BLUETOOTH_CONNECT", true)
+		}.ToArray();
+}
+#endif
 
 public partial class MainPage : ContentPage
 {
@@ -12,15 +25,25 @@ public partial class MainPage : ContentPage
 
 	private async void ScanClicked(object sender, EventArgs e)
 	{
-		var list = await ScanForDeviceList("Z1");
+		var list = await ScanForDeviceList("Z1"); // <--- put the name of the device you want to find and connect to here
 		if (list != null && list.Count > 0)
-			await DisplayAlert("Information", $"Found {list.Count} devices", "OK");
+		{
+			var peripheral = list[0];
+			peripheral.Connect(new ConnectionConfig { AutoConnect = false }); // <--- this will crash on Android
+		}
 		else
 			await DisplayAlert("Information", "No devices found", "OK");
 	}
 
 	public async Task<List<IPeripheral>> ScanForDeviceList(string nameContains, int timeout_ms = 5000, bool returnOnFirstFound = false)
 	{
+#if ANDROID
+		// quick and dirty way to get permissions on Android just for this test
+		if (await Permissions.CheckStatusAsync<Android12PlusPermissions>() != PermissionStatus.Granted)
+		{
+			await Permissions.RequestAsync<Android12PlusPermissions>();
+		}
+#endif
 		List<IPeripheral> deviceList = new List<IPeripheral>();
 
 		var bleManager = Shiny.Hosting.Host.Current.Services.GetService<IBleManager>();
